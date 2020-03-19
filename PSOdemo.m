f7dae3b0dfecfeddf4f0475d31154b1e84cf10d2
ZuoWen <44896590+Zuo-Wen@users.noreply.github.com>
2020-03-19 16:26:16 +0800

    Update PSOdemo.m

--- PSOdemo.m
+++ PSOdemo.m
@@ -1,32 +1,31 @@
 clc;clear;close all;
 tic;                          %程序运行计时开始
-E0=1.0e-20;                     %允许误差
+E0=1.0e-20;                   %允许误差
 MaxNum=100;                   %最大迭代次数
 narvs=2;                      %目标函数的自变量个数，解空间维数
-particlesize=100;              %粒子群规模
+particlesize=100;             %粒子群规模
 c1=2;                         %个体学习因子
 c2=2;                         %社会学习因子
 w=0.6;                        %惯性因子
-vmax=1;                     %粒子的最大飞翔速度
+vmax=1;                       %粒子的最大飞翔速度
 rand('state',100);
 x=-5 + 10*rand(particlesize,narvs); %粒子群的初始位置
 v=0.5*rand(particlesize,narvs);     %粒子群的初始速度
 %指定目标函数
-fitness=inline('20+x.^2+y.^2-10*(cos(2*pi.*x)+cos(2*pi.*y))','x','y');      %
-%fitness=inline('x.^2+y.^2','x','y');      
+%rastrigin function
+%fitness=inline('20+x.^2+y.^2-10*(cos(2*pi.*x)+cos(2*pi.*y))','x','y');   
+fitness=inline('x.^2+y.^2','x','y'); 
+
 
 globalbest_faval_list = zeros(MaxNum,1);
-%%%画出上述函数图像
+%画出上述函数图像
 [a,b]=meshgrid([-5:0.01:5]);
 z = fitness(a,b);
-figure();mesh(a,b,z)
-xlabel('X1');ylabel('X2');title('函数值')
-saveas(gcf, '函数值', 'png')
-figure(2);contour(a,b,z);hold on 
-xlabel('X1');ylabel('X2');title('等高线图')
-saveas(gcf, '等高线图', 'png')
+figure();meshc(a,b,z)
+xlabel('x1');ylabel('x2');zlabel('y');title('函数值及等高线图')
+saveas(gcf, '函数值及等高线图', 'png')
 pic_num = 1;
-%%%
+
 
 for i=1:particlesize   
     f(i)=fitness(x(i,1),x(i,2));
@@ -37,7 +36,7 @@ personalbest_faval=f;
 globalbest_x=personalbest_x(i,:);
 k=1;
 while k<=MaxNum
-    %%%画粒子群位置图
+    %画粒子群位置图
     figure(3);
     scatter(x(:,1),x(:,2)) ; 
     axis([-5,5 -5,5])
@@ -46,12 +45,12 @@ while k<=MaxNum
     I=frame2im(F);
     [I,map]=rgb2ind(I,256);
     if pic_num == 1
-        imwrite(I,map,'particleLaction.gif','gif', 'Loopcount',inf,'DelayTime',0.1);
+        imwrite(I,map,'particleLaction.gif','gif', 'Loopcount',inf,'DelayTime',1.5);
     else
         imwrite(I,map,'particleLaction.gif','gif','WriteMode','append','DelayTime',0.1);
     end
     pic_num = pic_num + 1;
-    %%%更新位置和速度
+    %更新位置和速度
     for i=1:particlesize
         f(i)=fitness(x(i,1),x(i,2));
         if f(i)<personalbest_faval(i)  %判断当前值是否是历史最佳
@@ -74,7 +73,7 @@ while k<=MaxNum
         x(i,:)=x(i,:)+v(i,:);
     end
 
-    %%% globalbest_faval curve
+    % globalbest_faval curve
     globalbest_faval_list(k,1) = globalbest_faval;
     
     if abs(globalbest_faval)<E0,break,end
